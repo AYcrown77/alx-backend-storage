88859ae69d28986993f53b0829e13289a8f7ea2b
AYcrown77 <aycrown77@gmail.com>
2023-04-10 12:43:00 +0000

    Fix typo error

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,5 +1,5 @@
 -- SQL script that creates a stored procedure ComputeAverageScoreForUser
-DELIMETER $;
+DELIMITER $;
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
 	DECLARE avg_score FLOAT;
@@ -8,5 +8,4 @@ BEGIN
 	UPDATE users
 	SET average_score = avg_score
 	WHERE id = user_id;
-END;
-$
+END;$
