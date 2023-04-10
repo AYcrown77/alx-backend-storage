899f2723b33293ab9d9fffef48582359e291d5c9
AYcrown77 <aycrown77@gmail.com>
2023-04-10 12:11:22 +0000

    Fix error

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,7 +1,5 @@
 -- SQL script that creates a trigger that decreases the quantity
 -- of an item after adding a new order.
-CREATE TRIGGER qty_decrease
-AFTER INSERT ON orders
-FOR EACH ROW
-UPDATE items SET items.quantity = items.quantity - NEW.number
-WHERE items.name = NEW.items_name;
+CREATE TRIGGER qty_decrease_items AFTER INSERT ON orders FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number
+WHERE name = NEW.items_name;
