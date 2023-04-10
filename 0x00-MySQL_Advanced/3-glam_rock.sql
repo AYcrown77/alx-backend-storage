7351ff1ef3b9666267a94948493477436bcbd587
AYcrown77 <aycrown77@gmail.com>
2023-04-10 11:57:55 +0000

    Correct typo

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- A script that lists all bands with glam rock
-SELECT bands_name, (IFNULL(split, '2022') - formed) AS lifespan
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
