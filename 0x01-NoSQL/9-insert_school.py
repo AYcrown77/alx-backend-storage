5f7f968a5ae2295643c0484c8b086bfa81f5a8dd
AYcrown77 <aycrown77@gmail.com>
2023-04-12 07:30:20 +0000

    Solve task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -2,7 +2,10 @@
 """inserts a new document in a collection based on kwargs"""
 
 
+import pymongo
+
+
 def insert_school(mongo_collection, **kwargs):
     """Returns the new _id"""
-    new = mongo_collection.insertOne(kwargs)
-    return new.get('_id')
+    new = mongo_collection.insert(kwargs)
+    return new
