f4afc40e1d27eda9dba02abb61dcfff95844bfa1
AYcrown77 <aycrown77@gmail.com>
2023-04-12 07:41:50 +0000

    Solve task 11

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -3,7 +3,7 @@
 import pymongo
 
 
-def schools_by_topic(mngo_collection, topic):
+def schools_by_topic(mongo_collection, topic):
     """search for the topic and return the school"""
     result_list = mongo_collection.find({"topics": topic})
     return result_list
